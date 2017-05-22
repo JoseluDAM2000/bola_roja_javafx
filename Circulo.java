d3b504105ffad8d11cd874506253c13419d87333
joselu <joseludam2000@gmail.com>
2017-05-22 12:45:39 +0200

    Ahora tiene una barra que se desplaza con las teclas

--- Circulo.java
+++ Circulo.java
@@ -14,7 +14,11 @@ import javafx.animation.Animation;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import java.util.Random;
+import javafx.geometry.Bounds;
+import javafx.scene.shape.Rectangle;
 
+import javafx.scene.input.KeyEvent;
 /**
  * Write a description of class Bola here.
  *
@@ -23,7 +27,20 @@ import javafx.event.EventHandler;
  */
 public class Circulo extends Application
 {
-    boolean enEjecucion = true;
+    private boolean enEjecucion;
+
+    private final int LADO=500;
+    private final int RADIO_CIRCULO = 25;
+
+    private final int ANCHO_RECTANGULO = 100;
+    private final int ALTO_RECTANGULO = 10;
+    private final int DISTANCIA_INFERIOR_RECTANGULO = 50;
+
+    private double deltaX;
+    private double deltaY;
+    
+    private boolean goEast, goWest;
+    private int movimientoRectangulo = 1;
 
     public static void main(String[] args){
         launch(args);
@@ -32,32 +49,82 @@ public class Circulo extends Application
     @Override
     public void start(Stage primaryStage)
     {
+        enEjecucion = true;
+        deltaX = 1;
+        deltaY = 1;
+
+        Random rnd = new Random();
+
         Pane panel = new Pane();
-        Circle circle = new Circle(250, 250, 25, Color.RED);
-        Scene escena = new Scene(panel, 500, 500);
+        Circle circle = new Circle(rnd.nextInt(LADO-RADIO_CIRCULO*2)+RADIO_CIRCULO, rnd.nextInt(LADO-RADIO_CIRCULO*2)+RADIO_CIRCULO, RADIO_CIRCULO, Color.RED);
+        Scene escena = new Scene(panel, LADO, LADO);
         panel.getChildren().add(circle);
         primaryStage.setScene(escena);
         primaryStage.show();
+
+        final Bounds bounds = panel.getBoundsInParent();
+
+        Rectangle rectangle = new Rectangle(ANCHO_RECTANGULO, ALTO_RECTANGULO, Color.BLUE);
+        rectangle.setX(bounds.getMaxX()/2 - ANCHO_RECTANGULO/2);
+        rectangle.setY(bounds.getMaxY() - DISTANCIA_INFERIOR_RECTANGULO);
+        rectangle.setArcWidth(20);
+        rectangle.setArcHeight(20);
+
+        panel.getChildren().add(rectangle);
+
+        escena.setOnKeyPressed(new EventHandler<KeyEvent>() {
+                @Override
+                public void handle(KeyEvent event) {
+                    switch (event.getCode()) {
+                        case LEFT:  
+                        goWest  = true; 
+                        goEast = false;
+                        break;
+                        case RIGHT: 
+                        goEast  = true;
+                        goWest = false;
+                        break;
+                    }
+                }
+            });
+        
+        
         
         KeyFrame kf = new KeyFrame(Duration.millis(10), (event) -> {
-            circle.setTranslateX(circle.getTranslateX()+1);
-            circle.setTranslateY(circle.getTranslateY()+1);
-        });
+                    final boolean atRightBorder = circle.getBoundsInParent().getMaxX() >= (bounds.getMaxX());
+                    final boolean atLeftBorder = circle.getBoundsInParent().getMinX() <= (bounds.getMinX());
+                    final boolean atBottomBorder = circle.getBoundsInParent().getMaxY() >= (bounds.getMaxY());
+                    final boolean atTopBorder = circle.getBoundsInParent().getMinY() <= (bounds.getMinY());
+                    if (atRightBorder || atLeftBorder) {
+                        deltaX *= -1;
+                    }
+                    if (atBottomBorder || atTopBorder) {
+                        deltaY *= -1;
+                    }
+
+                    circle.setLayoutX(circle.getLayoutX() + deltaX);
+                    circle.setLayoutY(circle.getLayoutY() + deltaY);
+                    
+                    if (goEast)  movimientoRectangulo = 1;
+                    if (goWest)  movimientoRectangulo = -1;
+                    rectangle.setLayoutX(rectangle.getLayoutX() + movimientoRectangulo);
+
+                });
         Timeline timeline = new Timeline(kf);
-        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
         Button btn = new Button("Parar");
         btn.setOnAction(event -> {
-            if(enEjecucion){
-                timeline.stop();
-            }else{
-                timeline.play();
-            }
-            modificarEjecucion();
-        });
+                if(enEjecucion){
+                    timeline.stop();
+                }else{
+                    timeline.play();
+                }
+                modificarEjecucion();
+            });
         panel.getChildren().add(btn);
     }
-    
+
     private void modificarEjecucion(){
         enEjecucion = !enEjecucion;
     }
